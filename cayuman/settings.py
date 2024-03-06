66c101d8f74a8bb79e56f02c1b5f81013caa7b1a
Roberto Alamos <ralamosm@gmail.com>
2024-03-06 13:05:53 -0300

    moved db conf to .env

--- cayuman/settings.py
+++ cayuman/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import json
 import os
 
 from dotenv import load_dotenv
@@ -99,12 +100,7 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "cayuman.db",
-    }
-}
+DATABASES = {"default": json.loads(os.getenv("DATABASE", "{}"))}
 
 
 # Password validation
