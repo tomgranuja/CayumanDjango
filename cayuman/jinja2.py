d55d101bed259bb9ca8c363a65c83ff9d518cafe
Roberto Alamos <ralamosm@gmail.com>
2024-03-11 16:52:23 -0300

    just moving non-students to admin and some minor improvements around templates

--- cayuman/jinja2.py
+++ cayuman/jinja2.py
@@ -1,4 +1,5 @@
 from django.template.context_processors import csrf
+from django.template.defaultfilters import date
 from django.urls import reverse
 from django.urls import reverse_lazy
 from jinja2 import Environment
@@ -7,4 +8,5 @@ from jinja2 import Environment
 def environment(**options):
     env = Environment(**options)
     env.globals.update({"url": reverse, "url_lazy": reverse_lazy, "csrf_token": lambda request: csrf(request)["csrf_token"]})
+    env.filters["date"] = date
     return env
