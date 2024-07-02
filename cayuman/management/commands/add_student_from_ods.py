44a146d77da9fb66e299f3296eb8a4a8af3cb589
Salvador Gaviota <tomgranuja@gmail.com>
2024-07-01 22:57:12 -0400

    Drop pandas and odfpy dependency from management scripts
    Use `csv.DictReader` instead of `pandas.DataFrame` in the scripts used
    to load initial period data to server. This way we can remove pandas
    and odfpy dependencies.

--- cayuman/management/commands/add_student_from_ods.py
+++ cayuman/management/commands/add_student_from_ods.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Helper script for initial members load from ods file."""
-import pandas as pd
+import csv
+
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
@@ -19,6 +20,15 @@ cycle_name = {
 }
 
 
+# Convert csv table to row-dict list, fix time coordinates and teacher
+def from_csv_table(fname):
+    with open(fname, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        t = [row for row in reader]
+        # for row in t:
+        return t
+
+
 class Command(BaseCommand):
     help = "Initial members load from ods file."
 
@@ -28,29 +38,29 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         filename = options["filename"]
         try:
-            t = pd.read_excel(filename)
+            t = from_csv_table(filename)
         except FileNotFoundError:
             raise CommandError('File "%s" does not exist' % filename)
 
         self.stdout.write(self.style.SUCCESS(f"Read {len(t)} entries"))
-        t = t[t.cycle.isin(cycle_name)]
+        t = [row for row in t if row["cycle"] in cycle_name]
         self.stdout.write(self.style.SUCCESS(f"Filtering to {len(t)} entries"))
         students_group, _ = Group.objects.get_or_create(name=settings.STUDENTS_GROUP)
-        for i, row in t.iterrows():
-            member, created = Member.objects.get_or_create(username=row.rut)
+        for row in t:
+            member, created = Member.objects.get_or_create(username=row["rut"])
             if created:
-                member.first_name = row.sfname
-                member.last_name = row.slname
-                member.email = row.email
-                member.set_password(row.rut.split("-")[0][-4:])
+                member.first_name = row["sfname"]
+                member.last_name = row["slname"]
+                member.email = row["email"]
+                member.set_password(row["rut"].split("-")[0][-4:])
                 member.save()
             else:
-                self.stdout.write(self.style.WARNING(f"Preexisting Member object: {member.first_name}, {member.username}"))
+                self.stdout.write(self.style.WARNING(f"Updating preexisting Member object groups: {member.first_name}, {member.username}"))
             # Force to student group
             member.groups.set([students_group])
 
             # Create student cycle relation if does not exist.
-            cycle, created = Cycle.objects.get_or_create(name=cycle_name[row.cycle])
+            cycle, created = Cycle.objects.get_or_create(name=cycle_name[row["cycle"]])
             try:
                 StudentCycle.objects.get(student=member, cycle=cycle)
             except StudentCycle.DoesNotExist:
--- cayuman/management/commands/add_teacher_from_ods.py
+++ cayuman/management/commands/add_teacher_from_ods.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
 """Helper script for initial members load from ods file."""
-import pandas as pd
+import csv
+
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
 
 from cayuman.models import Member
+
+
+def g_email_sanation(email):
+    host_list = ["gmail.com", "yahoo.com", "hotmail.com", "msn.com"]
+    email = str(email).strip()
+    if email != "" and "@" not in email:
+        email += "@comunidadeducativacayuman.cl"
+    if email.split("@")[-1].lower() in host_list:
+        email = email.lower()
+    return email
+
+
+# Convert csv table to row-dict list, fix email and active to boolean
+def from_csv_table(fname):
+    with open(fname, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        t = [row for row in reader]
+        for row in t:
+            if row["active"] == "1":
+                row["active"] = bool(row["active"])
+            else:
+                row["active"] = False
+            row["email"] = g_email_sanation(row["email"])
+        return t
 
 
 class Command(BaseCommand):
@@ -18,24 +43,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         filename = options["filename"]
         try:
-            t = pd.read_excel(filename, dtype={"active": "bool"})
+            t = from_csv_table(filename)
         except FileNotFoundError:
             raise CommandError('File "%s" does not exist' % filename)
 
         self.stdout.write(self.style.SUCCESS(f"Read {len(t)} entries"))
-        t = t[t.active]
+        t = [row for row in t if row["active"]]
         self.stdout.write(self.style.SUCCESS(f"Filtering to {len(t)} active entries"))
         teachers_group, _ = Group.objects.get_or_create(name=settings.TEACHERS_GROUP)
-        for i, row in t.iterrows():
-            member, created = Member.objects.get_or_create(username=row.rut)
+        for row in t:
+            member, created = Member.objects.get_or_create(username=row["rut"])
             if created:
-                member.first_name = row.gfname
-                member.last_name = row.glname
-                member.email = row.email
+                member.first_name = row["gfname"]
+                member.last_name = row["glname"]
+                member.email = row["email"]
                 member.is_staff = True
-                member.set_password(row.rut.split("-")[0][-4:])
+                member.set_password(row["rut"].split("-")[0][-4:])
                 member.save()
             else:
-                self.stdout.write(self.style.WARNING(f"Updating matching Member: {member.first_name}, {member.username}"))
-            # Force to techers group
+                self.stdout.write(self.style.WARNING(f"Updating matching Member groups: {member.first_name}, {member.username}"))
+            # Force to teachers group
             member.groups.set([teachers_group])
